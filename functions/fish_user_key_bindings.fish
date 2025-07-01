2b78776041a283f2bad7a4b46eb369211e9d7e2f
Takuto Komazaki <komazarari@gmail.com>
2025-07-01 11:03:25 +0900

    Ctrl+T fzf-file-widget

--- functions/fish_user_key_bindings.fish
+++ functions/fish_user_key_bindings.fish
@@ -1,7 +1,7 @@
-fzf_key_bindings
+# fzf_key_bindings
 function l --description alias\ l=rg\ --files\ --hidden\ --glob\ \"!.git/\*\"\ \|\ batcat\ \(fzf\ --preview\ \'batcat\ --style=numbers\ --color=always\ --line-range\ :\$LINES\ \{\}\'\)
   fzf --preview 'batcat --style=numbers --color=always --line-range :$LINES {}' | read -l result
-  [ "$result" ]; and batcat $result
+  [ "$result" ]; and batcat --plain $result
 end
 
 # Defined in - @ line 1
@@ -32,3 +32,36 @@ function __myawsprofile -d 'insert profile'
 end
 bind \eP '__myawsprofile'
 bind -M insert \eP '__myawsprofile'
+
+function __fzfcmd
+    set -q FZF_TMUX; or set FZF_TMUX 0
+    set -q FZF_TMUX_HEIGHT; or set FZF_TMUX_HEIGHT 40%
+    if [ $FZF_TMUX -eq 1 ]
+        echo "fzf-tmux -d$FZF_TMUX_HEIGHT"
+    else
+        echo "fzf"
+    end
+end
+
+function fzf-file-widget -d 'Insert selected file(s) at cursor'
+    set -l fzf_query (commandline --current-token)
+    [ -n "$fzf_query" ]; and set fzf_options --query="$fzf_query"
+
+    eval "$FZF_CTRL_T_COMMAND" | eval (__fzfcmd) -m --select-1 $fzf_options $FZF_CTRL_T_OPTS | while read -l r; set result $result $r; end
+
+    if [ -z "$result" ]
+        commandline -f repaint
+        return
+    else
+        commandline -t ""
+    end
+
+    for i in $result
+        commandline -it -- (string escape $i)
+        commandline -it -- ' '
+    end
+    commandline -f repaint
+end
+
+bind \ct fzf-file-widget
+bind -M insert \ct fzf-file-widget
