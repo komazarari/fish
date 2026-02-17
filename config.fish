49e23a65e27fec53042c7d0b220a08573935be13
Takuto Komazaki <komazarari@gmail.com>
2026-02-18 08:42:58 +0900

    upd

--- config.fish
+++ config.fish
@@ -72,3 +72,15 @@ if [ -f "$HOME/google-cloud-sdk/path.fish.inc" ]; . "$HOME/google-cloud-sdk/path
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+
+function vterm_printf;
+    if begin; [  -n "$TMUX" ]  ; and  string match -q -r "screen|tmux" "$TERM"; end
+        # tell tmux to pass the escape sequences through
+        printf "\ePtmux;\e\e]%s\007\e\\" "$argv"
+    else if string match -q -- "screen*" "$TERM"
+        # GNU screen (screen, screen-256color, screen-256color-bce)
+        printf "\eP\e]%s\007\e\\" "$argv"
+    else
+        printf "\e]%s\e\\" "$argv"
+    end
+end
